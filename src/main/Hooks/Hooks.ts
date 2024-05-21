12a96dad616ce5dc422c30b6f4faf5f571d65d53
Jayantagit <jayantamandal2006@gmail.com>
2024-05-21 11:20:50 +0530

    Report

--- src/main/Hooks/Hooks.ts
+++ src/main/Hooks/Hooks.ts
@@ -1,11 +1,12 @@
-import { BeforeAll, Before, AfterAll } from "@cucumber/cucumber";
+import { BeforeAll, Before, AfterAll, AfterStep, Status } from "@cucumber/cucumber";
 import { invokeBrowser } from "../../main/browser/Browsermanager";
 import { test, expect, chromium, Browser, BrowserContext, Page } from "@playwright/test";
 import Fixture from "./pageFixture";
+import { time } from "console";
 let browser: Browser;
 let browserContext: BrowserContext;
 let page: Page;
-export let contextFixture:Fixture;
+export let contextFixture: Fixture;
 
 BeforeAll(async function () {
     browser = await invokeBrowser();
@@ -14,11 +15,25 @@ BeforeAll(async function () {
 Before(async function () {
     browserContext = await browser.newContext();
     page = await browserContext.newPage();
-    contextFixture=new Fixture(browserContext,page);
-    
+    contextFixture = new Fixture(browserContext, page);
+
 })
-AfterAll(async function()
-{
+AfterAll(async function () {
+    // page.close();
     browser.close();
+})
+
+AfterStep(async function ({ result,pickle }) {
+    let img: Buffer;
+    const d = new Date();
+    let time = d.getTime();
+    let screenShotName = pickle.name +"_"+ time + ".png";
+    if (result.status == Status.FAILED) {
+        img = await contextFixture.getPage().screenshot({ path: "./test-result/screenshot/" + screenShotName, "type": "png" });
+        this.attach(img, 'image/png');
+
+    }
+
+
 })
 
